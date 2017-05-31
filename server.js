ee34b3d3de59c3bb3eb5e7178606d1d98fc976f6
Eduardo Santillana <edosantillana@gmail.com>
2017-05-31 13:19:43 +0200

    first commit

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var googleImages = require('google-images');
 
-var apiKey = 'AIzaSyCuWGz9GGxgu4YkmxFK1g-0I3h3Ynhq6M0';
-var cx = '004221433238695570707:uh-hddlyiho';
 var port = process.env.PORT || 8080;
-//var dburl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/test';
 var dburl = process.env.MONGOLAB_URI;
-var searches = null;
 apiKey = process.env.apiKey;
 cx = process.env.cx;
 var client = new googleImages(cx,apiKey);
+var searches = null;
 
 var app = express();
 
